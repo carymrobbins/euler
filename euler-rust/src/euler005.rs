c488dc9b68d99e9389e8d8fcccfb23e693230501
Cary Robbins <carymrobbins@gmail.com>
2020-10-12 22:43:05 -0500

    005: Refactoring

--- euler-rust/src/euler005.rs
+++ euler-rust/src/euler005.rs
@@ -24,42 +24,27 @@ pub fn divisible_by_all(min: u64, max: u64) -> u64 {
     if min < 1 {
         panic!("min cannot be less than 1: {} < 1", min);
     }
-    let mut ms = vec![];
-    for d in (min..(max+1)).rev() {
-        let mut add = true;
-        for m in ms.iter() {
-            if m % d == 0 {
-                add = false;
-                break;
-            }
-        }
-        if add {
-            ms.push(d);
+    if min == 1 && max == 1 {
+        return 1;
+    }
+    let start = if min == 1 { 2 } else { min };
+    let range = || start..=max;
+    let mut multipliers = vec![];
+    for d in range().rev() {
+        if multipliers.iter().all(|m| m % d != 0) {
+            multipliers.push(d);
         }
     }
-    let mut p = ms.iter().product();
-    for m in min..(max+1) {
-        if m == 1 {
-            continue;
-        }
+    let mut result = multipliers.iter().product();
+    for d in range() {
         loop {
-            let p2 = p / m;
-            let mut ok = true;
-            for n in min..(max+1) {
-                if n == 1 {
-                    continue;
-                }
-                if p2 % n != 0 {
-                    ok = false;
-                    break;
-                }
-            }
-            if ok {
-                p = p2;
+            let q = result / d;
+            if range().all(|n| q % n == 0) {
+                result = q;
             } else {
                 break;
             }
         }
     }
-    p
+    result
 }
