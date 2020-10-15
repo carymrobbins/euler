a9f654892dd828eb688345c25bff5176973107b5
Cary Robbins <carymrobbins@gmail.com>
2020-10-15 13:55:36 -0500

    Improve rust infra

--- euler-rust/src/main.rs
+++ euler-rust/src/main.rs
@@ -1,8 +1,63 @@
+use std::collections::BTreeMap;
+use std::env;
+use std::process;
+use std::string::String;
+use std::vec::Vec;
+
 mod euler005;
 mod euler006;
 mod euler007;
 mod run;
 
+fn mk_euler_tasks() -> BTreeMap<u16, fn()> {
+    let mut res: BTreeMap<u16, fn()> = BTreeMap::new();
+    res.insert(005, euler005::main);
+    res.insert(006, euler006::main);
+    res.insert(007, euler007::main);
+    res
+}
+
 fn main() {
-    euler007::main();
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0); // Skip the program name.
+    for arg in args.iter() {
+        if arg == "--help" {
+            println!("{}", USAGE);
+            process::exit(0);
+        }
+    }
+    if args.is_empty() {
+        match env::var_os("EULER") {
+            None => abort(format!("Missing arguments\n{}", USAGE)),
+            Some(os_str) => match os_str.to_str() {
+                None => abort(format!("Failed to decode EULER env var")),
+                Some(var) => args = var.split_whitespace().map(|x| x.to_string()).collect(),
+            },
+        }
+    }
+
+    let euler_tasks = mk_euler_tasks();
+    let get_task = |k: &String| match k.parse::<u16>() {
+        Err(_) => abort(format!("Number not accepted: {}", k)),
+        Ok(n) => match euler_tasks.get(&n) {
+            None => abort(format!("Euler not found: {:03}", n)),
+            Some(f) => *f,
+        },
+    };
+
+    let tasks: Vec<fn()> = if args == ["all"] {
+        euler_tasks.values().map(|f| *f).collect()
+    } else {
+        args.iter().map(get_task).collect()
+    };
+
+    tasks.iter().for_each(|f| f());
+    println!("Complete!");
+}
+
+static USAGE: &str = "Usage: euler-rust <000> [<000> ...]";
+
+fn abort<T>(msg: String) -> T {
+    eprintln!("{}", msg);
+    process::exit(1)
 }
--- euler-rust/src/run.rs
+++ euler-rust/src/run.rs
@@ -1,12 +1,12 @@
 use std::time::SystemTime;
 
-pub fn run<F : FnOnce() -> ()>(name: &str, f: F) {
-    println!("{}", name);
+pub fn run<F: FnOnce() -> ()>(name: &str, f: F) {
+    println!("Running Euler {} ...", name);
     let start = SystemTime::now();
     f();
-    let s = start.elapsed().map_or(
-        "?".to_string(),
-        |d| d.as_secs_f32().to_string()
-    );
-    println!("OK: Completed in {} seconds", s);
+    let s = start
+        .elapsed()
+        .map_or("?".to_string(), |d| format!("{:.6}", d.as_secs_f32()));
+    println!("  ✔ OK: Completed in {} seconds", s);
+    println!("----------------------------------------");
 }
